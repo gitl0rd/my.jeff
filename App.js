4da03cb69abddb6125432717aef1c8cae6378c02
julian.davies <julianjcdavies@gmail.com>
2018-08-09 17:37:21 +1000

    screens

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LogInScreen from './Screens/LogIn';
+import HomeScreen from './Screens/Home';
+
+const RootStack = createStackNavigator(
+  {
+    LogIn: LogInScreen,
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'LogIn',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#0065a8',
+      },
+      headerTintColor: '#fff',
+    },
+  }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
